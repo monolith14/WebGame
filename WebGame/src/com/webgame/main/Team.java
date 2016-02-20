66821159b4b81375b71efc8eae6dc265351a012f
Mihail Mihaylov <monolith14@gmail.com>
2016-02-20 09:49:08 +0200

    Team class

---
+++ WebGame/src/com/webgame/main/Team.java
@@ -0,0 +1,83 @@
+package com.webgame.main;
+
+public class Team {
+	private int id;
+	private int uid;
+	private String name;
+	private int stat1;
+	private int stat2;
+	private int stat3;
+	private int stat4;
+	private int stat;
+	
+	public Team(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStat1() {
+		return stat1;
+	}
+
+	public void setStat1(int stat1) {
+		this.stat1 = stat1;
+	}
+
+	public int getStat2() {
+		return stat2;
+	}
+
+	public void setStat2(int stat2) {
+		this.stat2 = stat2;
+	}
+
+	public int getStat3() {
+		return stat3;
+	}
+
+	public void setStat3(int stat3) {
+		this.stat3 = stat3;
+	}
+
+	public int getStat4() {
+		return stat4;
+	}
+
+	public void setStat4(int stat4) {
+		this.stat4 = stat4;
+	}
+
+	public int getStat() {
+		return stat;
+	}
+
+	public void setStat(int stat) {
+		this.stat = stat;
+	}
+	
+	
+
+}
